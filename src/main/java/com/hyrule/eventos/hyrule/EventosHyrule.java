b52cc3d4f9027415f6dccd4e6497e2f53db139ec
alex-domingo <domingoartola09@outlook.es>
2025-08-13 01:51:52 -0600

    Instancia DBConnection

--- src/main/java/com/hyrule/eventos/hyrule/EventosHyrule.java
+++ src/main/java/com/hyrule/eventos/hyrule/EventosHyrule.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.hyrule.eventos.hyrule;
+
+import com.hyrule.eventos.hyrule.dbconnection.DBConnection;
 
 /**
  *
@@ -12,5 +13,7 @@ public class EventosHyrule {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        DBConnection connection = new DBConnection();
+        connection.connect();
     }
 }
