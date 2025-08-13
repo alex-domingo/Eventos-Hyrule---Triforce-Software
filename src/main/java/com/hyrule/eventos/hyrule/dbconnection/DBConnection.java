2c1b31b7990aa826583dfe085c1d825b19f7fe35
alex-domingo <domingoartola09@outlook.es>
2025-08-13 02:00:07 -0600

    Realizamos la conexion a la RDBMS

---
+++ src/main/java/com/hyrule/eventos/hyrule/dbconnection/DBConnection.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hyrule.eventos.hyrule.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Ludvi
+ */
+public class DBConnection {
+
+    private static final String IP = "localhost";
+    private static final int PUERTO = 3306;
+    private static final String SCHEMA = "eventos_hyrule_esquema";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "7777";
+
+    public static final String URL = "jdbc:mysql://"
+            + IP + ":" + PUERTO + "/" + SCHEMA;
+
+    public static final String URL_FATAL = "jdbc:mysql://"
+            + IP + ":" + PUERTO + "/" + SCHEMA + "?allowMultiQueries=true";
+
+    private Connection connection;
+
+    public void connect() {
+        System.out.println("URL de conexion: " + URL);
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println("Esquema: " + connection.getSchema());
+            System.out.println("Catalogo: " + connection.getCatalog());
+
+        } catch (SQLException e) {
+
+            // Manejamos la exception
+            System.out.println("Error al conectarse");
+            e.printStackTrace();
+        }
+    }
+}
