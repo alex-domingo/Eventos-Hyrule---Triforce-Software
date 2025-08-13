167a4c0a2a900c5f7abc819a97921402049ba1ff
alex-domingo <domingoartola09@outlook.es>
2025-08-13 00:40:39 -0600

    Primera carga de archivos a mi repositorio de github

---
+++ src/main/java/com/hyrule/eventos/hyrule/EventosHyrule.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hyrule.eventos.hyrule;
+
+/**
+ *
+ * @author Ludvi
+ */
+public class EventosHyrule {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
