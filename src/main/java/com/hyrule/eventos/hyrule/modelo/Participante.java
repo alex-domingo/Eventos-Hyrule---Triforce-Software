3a3ff2d75dfbf601517b04832ce09ea534813da9
alex-domingo <domingoartola09@outlook.es>
2025-08-13 23:42:32 -0600

    Creacion de objetos para almacenar datos de participante

---
+++ src/main/java/com/hyrule/eventos/hyrule/modelo/Participante.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hyrule.eventos.hyrule.modelo;
+
+/**
+ *
+ * @author Ludvi
+ */
+public class Participante {
+
+    private String correo;          // PK
+    private String nombreCompleto;  // <= 60
+    private String tipo;            // 'estudiante','profesional','invitado'
+    private String institucion;     // <= 150
+
+    public Participante() {
+    }
+
+    public Participante(String correo, String nombreCompleto, String tipo, String institucion) {
+        this.correo = correo;
+        this.nombreCompleto = nombreCompleto;
+        this.tipo = tipo;
+        this.institucion = institucion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+}
